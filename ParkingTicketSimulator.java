baf332f9ebe0f8130914a64a26e1326f57e3548f
Ifiok  Sunday Edemidiong <117485944+ifiki8236@users.noreply.github.com>
2023-04-20 22:42:49 -0400

    Update ParkingTicketSimulator.java
    I placed options into 2D and single dimensional arrays. I have to go back and fix the access types due to static types messing with my car objects and their results.

--- ParkingTicketSimulator.java
+++ ParkingTicketSimulator.java
@@ -2,19 +2,32 @@
 
 public class ParkingTicketSimulator {
     public static void main(String args[]) {
-        String modelOfCar = "nissan", makeOfCar = "altima", colorOfCar = "blue", carPlate = "EY67354";
-        int minutesParked = 180;
-        int purchasedMin = 60; 
-        String policeName = "Michael Vick";
-        int policeBadge = 59784;
+        String [] [] vehicle_information = {{"nissan", "altima", "white-gray", "TRUMP24"},{"toyota", "corolla", "dark blue", "TUF5602"},{"dodge", "charger", "red and gold", "EY67354"}}; //makeOfCar = {"altima"}, colorOfCar = {"blue"}, carPlate = "EY67354";
+        int[] minutesParked = {3645, 482, 65};
+        int[] purchasedMin = {1000, 543, 60}; 
+        String[] policeName = {"Michael Vick", "Sarai McKenneth Bartley", "Leroy Jenkis"};
+        int[] policeBadge = {597, 802, 420};
 
         //ParkedCar car = new ParkedCar(modelOfCar, makeOfCar, colorOfCar, carPlate, minutesParked);
-        PoliceOfficer police = new PoliceOfficer(policeBadge, policeName);
-        ParkingTicket car = new ParkingTicket(modelOfCar, makeOfCar, colorOfCar, carPlate, minutesParked, police, purchasedMin);
+        PoliceOfficer police1 = new PoliceOfficer(policeBadge[0], policeName[0]);
+        ParkingTicket car1 = new ParkingTicket(vehicle_information[0][0], vehicle_information[0][1], vehicle_information[0][2], vehicle_information[0][3], minutesParked[0], police1, purchasedMin[0]);
+        // PoliceOfficer police2 = new PoliceOfficer(policeBadge[1], policeName[1]);
+        // ParkingTicket car2 = new ParkingTicket(vehicle_information[1][0], vehicle_information[1][1], vehicle_information[1][2], vehicle_information[1][3], minutesParked[1], police2, purchasedMin[1]);
+        // PoliceOfficer police3 = new PoliceOfficer(policeBadge[0], policeName[0]);
+        // ParkingTicket car3 = new ParkingTicket(vehicle_information[2][0], vehicle_information[2][1], vehicle_information[2][2], vehicle_information[2][3], minutesParked[2], police3, purchasedMin[2]);
 
         //System.out.println(ticketInfo.getOfficerBadge());
-        String theFinalVerdict = PoliceOfficer.issueTicket(car, police);
-        System.out.println(theFinalVerdict);
+        String theFinalVerdict1 = PoliceOfficer.issueTicket(car1, police1);
+        // String theFinalVerdict2 = PoliceOfficer.issueTicket(car2, police2);
+        // String theFinalVerdict3 = PoliceOfficer.issueTicket(car3, police3);
+
+        System.out.println("First ticket:");
+        System.out.println(theFinalVerdict1);
+        // System.out.println("\nFirst ticket:");
+        // System.out.println(theFinalVerdict2);
+        // System.out.println("\nFirst ticket:");
+        // System.out.println(theFinalVerdict3);
+
 
     }
 }
